2d0d2db4a4218f59e04642005ebb1652cf409982
iputucaganesha <iputucaganesha@gmail.com>
2023-05-16 15:47:27 +0700

    Format process change
    Change the format from .png to .jpg

--- compress.py
+++ compress.py
@@ -2,8 +2,8 @@ from PIL import Image
 import os
 import csv
 
-input_dir = 'pictures' # directory containing the images to be compressed
-output_dir = 'output_images' # directory where the compressed images will be saved
+input_dir = '/content/pillow-compress-lossless/pictures' # directory containing the images to be compressed
+output_dir = '/content/pillow-compress-lossless/output_images' # directory where the compressed images will be saved
 csv_file = 'compression_results.csv' # CSV file where the compression results will be saved
 
 if not os.path.exists(output_dir):
@@ -14,14 +14,14 @@ with open(csv_file, 'w') as f:
     writer.writerow(['Image', 'Original Size (bytes)', 'Compressed Size (bytes)', 'Compression Ratio'])
 
     for filename in os.listdir(input_dir):
-        if filename.endswith('.png'):
+        if filename.endswith('.jpg'):
             input_path = os.path.join(input_dir, filename)
             output_path = os.path.join(output_dir, filename)
 
             original_size = os.stat(input_path).st_size
 
             image = Image.open(input_path)
-            image.save(output_path, 'PNG')
+            image.save(output_path, 'JPEG')
 
             compressed_size = os.stat(output_path).st_size
             compression_ratio = compressed_size / original_size
